b9bde670b19c288fa37ff845806720bff02c30a9
zooregi51 <zooregi51@gmail.com>
2024-12-16 15:00:24 +0900

    2차 수정

--- ex05/src/main/java/ecount/quotation/contoller/quotationContoller.java
+++ ex05/src/main/java/ecount/quotation/contoller/quotationContoller.java
@@ -1,8 +1,15 @@
 package ecount.quotation.contoller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
 
 @Controller
+@Log4j
+@RequestMapping("/qutotation/*")
+@AllArgsConstructor
 public class quotationContoller {
 
 }

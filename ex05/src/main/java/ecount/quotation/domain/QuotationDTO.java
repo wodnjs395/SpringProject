84565c728ca6b11f56af012a6f6a628d30aff038
zooregi51 <zooregi51@gmail.com>
2024-12-18 15:41:40 +0900

    import 추가

--- ex05/src/main/java/ecount/quotation/domain/QuotationDTO.java
+++ ex05/src/main/java/ecount/quotation/domain/QuotationDTO.java
@@ -3,17 +3,21 @@ package ecount.quotation.domain;
 import java.sql.Timestamp;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class QuotationDTO {
 
 	private String acc_uuid;
 	private int acc_number;
 	private int clientNum;
 	private int storageId;
-	private int projectNum;
-	private int status;
+	private int projectNum; //Nullable
+	private String status;
 	private Timestamp created_at;
 	private Timestamp modified_at;
-	private Timestamp deleted_at;
+	private Timestamp deleted_at; //Nullable
 }
